232379c5ab50418453f96e9cc3c75be325ee1f07
hpwang <hpwang@whu.edu.cn>
2024-10-23 20:44:45 +0800

    [Fooocus weights]

--- download_weights.sh
+++ download_weights.sh
@@ -8,11 +8,14 @@ wget -P tools/Fooocus/models/checkpoints/ https://huggingface.co/lllyasviel/fav_
 # Fooocus lora model
 wget -P tools/Fooocus/models/loras/ https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_offset_example-lora_1.0.safetensors 
 # Fooocus inpaint model
-wget -P tools/Fooocus/models/inpaint/ https://huggingface.co/lllyasviel/fooocus_inpaint/blob/main/inpaint_v26.fooocus.patch 
+wget -P tools/Fooocus/models/inpaint/ https://huggingface.co/lllyasviel/fooocus_inpaint/resolve/main/inpaint_v26.fooocus.patch?download=true
+mv tools/Fooocus/models/inpaint/inpaint_v26.fooocus.patch?download=true tools/Fooocus/models/inpaint/inpaint_v26.fooocus.patch
 # Fooocus Prompt-Extension
-wget -P tools/Fooocus/models/prompt_expansion/fooocus_expansion/pytorch_model.bin  https://huggingface.co/lllyasviel/misc/blob/main/fooocus_expansion.bin 
+wget -P tools/Fooocus/models/prompt_expansion/fooocus_expansion/ https://huggingface.co/lllyasviel/misc/resolve/main/fooocus_expansion.bin?download=true
+mv tools/Fooocus/models/prompt_expansion/fooocus_expansion/fooocus_expansion.bin?download=true tools/Fooocus/models/prompt_expansion/fooocus_expansion/pytorch_model.bin
 # Foooocus Upscale
-wget -P tools/Fooocus/models/unscale_models/ https://huggingface.co/lllyasviel/misc/blob/main/fooocus_upscaler_s409985e5.bin 
+wget -P tools/Fooocus/models/unscale_models/ https://huggingface.co/lllyasviel/misc/resolve/main/fooocus_upscaler_s409985e5.bin?download=true
+mv tools/Fooocus/models/unscale_models/fooocus_upscaler_s409985e5.bin?download=true tools/Fooocus/models/unscale_models/fooocus_upscaler_s409985e5.bin
 
 # Depth Pro
 wget -P tools/DepthPro/checkpoints https://ml-site.cdn-apple.com/models/depth-pro/depth_pro.pt 
@@ -21,4 +24,4 @@ wget -P tools/OneFormer/checkpoints https://shi-labs.com/projects/oneformer/ade2
 
 # stable diffusion - lcm
 # wget -P tools/StableDiffusion/ckpt  # original SD1.5 ckpt will be automatically downloaded from https://huggingface.co/stable-diffusion-v1-5/stable-diffusion-v1-5
-wget -P tools/StableDiffusion/lcm_ckpt https://huggingface.co/latent-consistency/lcm-lora-sdv1-5/resolve/main/pytorch_lora_weights.safetensors
+wget -P tools/StableDiffusion/ckpt https://huggingface.co/latent-consistency/lcm-lora-sdv1-5/resolve/main/pytorch_lora_weights.safetensors
